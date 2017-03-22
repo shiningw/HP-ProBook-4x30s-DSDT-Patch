4a4e3e97e72628b12f5615df14fd4160d91fd84b
jiaxin,huang <freefallbenson@gmail.com>
2017-03-22 09:37:30 +0800

    added 840g1-specific directory

--- myinstall.sh
+++
@@ -1,3 +0,0 @@
-for name in "SSDT-HACK.aml SSDT-XOSI.aml SSDT-EH01.aml SSDT-EH02.aml SSDT-XHC.aml SSDT-LPC.aml SSDT-SATA.aml SSDT-SMBUS.aml SSDT-PNLF.aml SSDT-PRW.aml SSDT-LANC_PRW.aml SSDT-BATC.aml";do sudo cp -av $name /volumes/efi/efi/clover/acpi/patched/;done
-
-cp -av SSDT-IGPU.aml /volumes/efi/efi/clover/acpi/patched/
